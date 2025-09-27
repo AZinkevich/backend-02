7d1b44837c0908a5e523e9971ebdb6b7d7a8f6cf
AZinkevich <azinkevich@mail.ru>
2025-09-27 10:26:43 +0300

    update

--- src/index.js
+++ src/index.js
@@ -18,36 +18,24 @@ const server = http.createServer((request, response) => {
       response.end("Enter a name");
     }
     return;
-  } else if (request.url === "/?users") {
-    response.status = 200;
-    response.statusMessage = "OK";
-    response.header = "Content-Type: application/json";
-    response.write(getUsers());
-    response.end();
+  } else if (url.searchParams.has("users")) {
+    response.writeHead(200, { "Content-Type": "application/json" });
+    response.end(getUsers());
     return;
   } else if (request.url === "/") {
-    response.status = 200;
-    response.statusMessage = "OK";
-    response.header = "Content-Type: text/plain";
-    response.write("Hello, World!");
+    response.writeHead(200, { "Content-type": "text/plain" });
+    response.end("Hello, World!");
+    return;
+  } else if (request.url === "/favicon.ico") {
+    response.writeHead(200, { "Content-Type": "image/x-icon" });
     response.end();
     return;
   } else {
-    response.statusCode = 500;
-    response.statusMessage = "Server Error";
-    response.header = "Content-Type: text/plain";
-    response.write("");
-    response.end();
+    response.writeHead(500, { "Content-type": "text/plain" });
+    response.end("");
 
     return;
   }
-
-  // Написать обработчик запроса:
-  // - Ответом на запрос `?hello=<name>` должна быть **строка** "Hello, <name>.", код ответа 200
-  // - Если параметр `hello` указан, но не передано `<name>`, то ответ **строка** "Enter a name", код ответа 400
-  // - Ответом на запрос `?users` должен быть **JSON** с содержимым файла `data/users.json`, код ответа 200
-  // - Если никакие параметры не переданы, то ответ **строка** "Hello, World!", код ответа 200
-  // - Если переданы какие-либо другие параметры, то пустой ответ, код ответа 500
 });
 
 server.listen(3003, () => {
